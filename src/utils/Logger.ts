ecf611643f75d5f6976c4af598d8153e0fa6aa35
m.buczek <m.buczek@bsgroup.eu>
2022-03-31 11:54:27 +0200

    feat: add logger

---
+++ src/utils/Logger.ts
@@ -0,0 +1,20 @@
+export class Logger {
+    static instance: Logger;
+
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new Logger();
+        }
+        return this.instance;
+    }
+    info(tag: string, message: string, context?: any) {
+        console.log(`[${tag}] : ${message}`, { context });
+    }
+
+    debug(tag: string, message: string, context?: any) {
+        console.debug(`[${tag}] : ${message}`, { context });
+    }
+    error(tag: string, message: string, context?: any) {
+        console.error(`[${tag}] : ${message}`, { context });
+    }
+}
