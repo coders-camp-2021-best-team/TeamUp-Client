7aa61a1e0c878b916bb495c3eb777b8676d119c5
Mateusz Buczek <85763715+T0ffik@users.noreply.github.com>
2022-03-31 17:34:55 +0200

    feat: add routing (#15)
    * feat: add routing
    
    * refacor: comments included
    
    * refactor: include comments
    
    * refactor: correct comment
    
    Co-authored-by: m.buczek <m.buczek@bsgroup.eu>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import { Router } from './Router';
+
 export const App = () => {
-    return (
-        <div className='app'>
-            <h1>Hello, world!</h1>
-        </div>
-    );
+    return <Router />;
 };
---
+++ src/routes/Routes.ts
@@ -0,0 +1,14 @@
+export const ROUTES = {
+    LOGIN: '/login',
+    REGISTER: '/register',
+    HOME: '/',
+    FEED: '/feed',
+    PROFILE: '/profile',
+    CHAT: '/chat',
+    SEARCH: '/search',
+    POST: '/post',
+    RESET_PASSWORD: '/reset-password',
+    CONFIRMATION_EMAIL: '/confirmation-email',
+    NOT_FOUND: '/not-found',
+    ERROR: '*'
+};
