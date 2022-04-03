0a79878537ddaf66e45dd09d6a89868dc3a29819
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-04-03 15:59:05 +0200

    prepare theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { ThemeProvider } from '@emotion/react';
+import { createTheme } from '@mui/material/styles';
+
 import { Router } from './Router';
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#6C56F9',
+            light: '#B2A7FC',
+            dark: '#3415F7'
+        },
+        background: {
+            default: '#1A1B22'
+        }
+    }
+});
+
 export const App = () => {
-    return <Router />;
+    return (
+        <ThemeProvider theme={theme}>
+            <Router />
+        </ThemeProvider>
+    );
 };
