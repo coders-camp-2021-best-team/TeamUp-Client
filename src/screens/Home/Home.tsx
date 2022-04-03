42eda8d98aeeab49370251193e7a7e803b4e6649
Konrad <trakon1e@gmail.com>
2022-04-03 17:17:32 +0200

    refactor: Use custom hook for testing

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,17 @@
 import { NavLink } from 'react-router-dom';
 
+import { useOurMedia } from '../../hooks';
 import { ROUTES } from '../../routes/Routes';
 
 export const Home = () => {
+    const phoneOnly = useOurMedia();
+    const tabletSize = useOurMedia('tablet', 'min');
     return (
         <>
             <NavLink to={ROUTES.FEED}>FEED</NavLink>
             <div>Home</div>
+            {phoneOnly && <p>Phone Size Max</p>}
+            {tabletSize && <p>Tablet Size Min</p>}
         </>
     );
 };
