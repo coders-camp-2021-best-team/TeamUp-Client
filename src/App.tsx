a04c73b610bf86c1af863f17a19adfbe7cc70846
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-04-03 20:46:50 +0200

    create config folder and add theme.ts with prepare theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
 import { ThemeProvider } from '@emotion/react';
-import { createTheme } from '@mui/material/styles';
 
+import { theme } from './config/theme';
 import { Router } from './Router';
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#6C56F9',
-            light: '#B2A7FC',
-            dark: '#3415F7'
-        },
-        background: {
-            default: '#1A1B22'
-        }
-    }
-});
 
 export const App = () => {
     return (
