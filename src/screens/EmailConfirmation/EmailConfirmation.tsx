c9ad952c0a37d984ab0e63873f66c77035aade7b
Mariusz-web <78812587+Mariusz-web@users.noreply.github.com>
2022-04-07 21:07:09 +0200

    creat mail confirmation

--- src/screens/EmailConfirmation/EmailConfirmation.tsx
+++ src/screens/EmailConfirmation/EmailConfirmation.tsx
@@ -1,3 +1,34 @@
+import CheckRoundedIcon from '@mui/icons-material/CheckRounded';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
 export const EmailConfirmation = () => {
-    return <div>EmailConfirmation</div>;
+    return (
+        <Box
+            sx={{
+                marginTop: 15,
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                textAlign: 'center'
+            }}
+        >
+            <Avatar
+                sx={{
+                    m: 1,
+                    bgcolor: 'primary.main',
+                    width: '50px',
+                    height: '50px'
+                }}
+            >
+                <CheckRoundedIcon
+                    sx={{ fontSize: '36px', color: 'text.primary' }}
+                />
+            </Avatar>
+            <Typography component='h1' variant='h6' color='background.paper'>
+                Congrats, successfully confirmation mail
+            </Typography>
+        </Box>
+    );
 };
