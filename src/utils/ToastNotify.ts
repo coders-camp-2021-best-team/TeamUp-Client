dfb7592bfff5321e3f27ea5b413db00fc23d4950
m.buczek <m.buczek@bsgroup.eu>
2022-04-11 10:53:05 +0200

    pls work

---
+++ src/utils/ToastNotify.ts
@@ -0,0 +1,43 @@
+import 'react-toastify/dist/ReactToastify.css';
+
+import { toast, ToastOptions } from 'react-toastify';
+
+const succesStyles: ToastOptions = {
+    position: 'bottom-right',
+    autoClose: 3000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: false,
+    progress: undefined
+};
+
+const errorStyles: ToastOptions = {
+    position: 'bottom-right',
+    autoClose: 5000,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: false,
+    draggable: false,
+    progress: undefined
+};
+
+export const toastNotify = (status: number, text?: string) => {
+    switch (status) {
+        case 400:
+            return toast.error(
+                text || 'Bad request, try something else!',
+                errorStyles
+            );
+        case 401:
+            return toast.error(text || 'Unauthorized');
+        case 404:
+            return toast.error(text || 'Not Found');
+        case 500:
+            return toast.info(text || 'Internal server error, try again later');
+        case 200:
+            return toast.success(text || 'Success!!', succesStyles);
+        default:
+            return toast.error(text || 'Unknown error');
+    }
+};
