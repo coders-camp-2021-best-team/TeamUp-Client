ce3e8fd8a8177c52a2a0d4c0eafe845b7d1a09c2
Maciej Opaliński <poprostumieciek@gmail.com>
2022-04-11 10:19:43 +0200

    fix: Fix custom logger and env vars

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,22 +1,15 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 export class Logger {
-    static info(tag: string, message: string, context?: any) {
-        if (process.env.REACT_APP_DEVELOPER_MODE !== 'true') {
-            return;
-        }
-        console.log(`[${tag}] : ${message}`, { context });
+    static debug(...params: unknown[]) {
+        if (process.env.REACT_APP_DEBUG !== 'true') return;
+
+        console.debug(...params);
     }
 
-    static debug(tag: string, message: string, context?: any) {
-        if (process.env.REACT_APP_DEVELOPER_MODE !== 'true') {
-            return;
-        }
-        console.debug(`[${tag}] : ${message}`, { context });
+    static info(...params: unknown[]) {
+        console.log(...params);
     }
-    static error(tag: string, message: string, context?: any) {
-        if (process.env.REACT_APP_DEVELOPER_MODE !== 'true') {
-            return;
-        }
-        console.error(`[${tag}] : ${message}`, { context });
+
+    static error(...params: unknown[]) {
+        console.error(...params);
     }
 }
