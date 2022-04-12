86afd6e782e88ad1587ce4c59b23325bfb3931fa
Konrad <trakon1e@gmail.com>
2022-04-12 20:19:53 +0200

    refactor: Add TODO comment

--- src/components/Profile/UserProfile.tsx
+++ src/components/Profile/UserProfile.tsx
@@ -2,6 +2,7 @@ import { useOurMedia } from '../../hooks';
 import { DesktopProfile } from './desktop';
 import { MobileProfile } from './mobile';
 
+//TODO connect screen to API
 export const UserProfile = () => {
     const isPhoneSize = useOurMedia();
     return <>{isPhoneSize ? <MobileProfile /> : <DesktopProfile />}</>;
