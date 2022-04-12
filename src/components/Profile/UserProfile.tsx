cd9c4f3f459c89e2409c6bf21bbd5547985f04e5
Konrad <trakon1e@gmail.com>
2022-04-12 20:45:07 +0200

    refactor: Put mobile and desktop components to one file

--- src/components/Profile/UserProfile.tsx
+++ src/components/Profile/UserProfile.tsx
@@ -1,9 +1,65 @@
+import { Avatar, Box, Typography } from '@mui/material';
+
 import { useOurMedia } from '../../hooks';
-import { DesktopProfile } from './desktop';
-import { MobileProfile } from './mobile';
+import { Description } from './description';
 
 //TODO connect screen to API
 export const UserProfile = () => {
     const isPhoneSize = useOurMedia();
-    return <>{isPhoneSize ? <MobileProfile /> : <DesktopProfile />}</>;
+    return (
+        <>
+            {isPhoneSize ? (
+                <>
+                    <Box
+                        component='img'
+                        alt='Profile Picture'
+                        src='https://source.unsplash.com/random'
+                        width='100%'
+                        height='60vh'
+                        sx={{
+                            borderRadius: '6px'
+                        }}
+                    />
+                    <Typography
+                        variant='h5'
+                        sx={{ flexGrow: 1 }}
+                        margin='2vh 5.5vw'
+                        color='white'
+                    >
+                        Jan Kowalski, 22
+                    </Typography>
+                    <Description />
+                </>
+            ) : (
+                <Box
+                    sx={{
+                        display: 'flex',
+                        minHeight: '100vh',
+                        marginTop: '3vh'
+                    }}
+                >
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            padding: '0 3vh'
+                        }}
+                    >
+                        <Avatar
+                            src='https://source.unsplash.com/random'
+                            alt='Profile Picture'
+                            sx={{
+                                width: 150,
+                                height: 150,
+                                marginBottom: '5vh'
+                            }}
+                        />
+                        <Typography color='white'>Jan Kowalski, 22</Typography>
+                    </Box>
+                    <Description />
+                </Box>
+            )}
+        </>
+    );
 };
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,4 +1,2 @@
 export * from './description';
-export * from './desktop';
-export * from './mobile';
 export * from './UserProfile';
