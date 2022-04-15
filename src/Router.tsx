65887697f5906fddcd30f573f1b501d605dcce1a
Maciej Opaliński <poprostumieciek@gmail.com>
2022-04-15 16:48:01 +0200

    fix: Fix `Router`
    #26

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import { ProtectedRoute } from './components';
+import { LoggedOutRoute } from './components/LoggedOutRoute';
 import { ROUTES } from './routes/Routes';
 import {
     BaseScreen,
@@ -21,22 +22,32 @@ export const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path={ROUTES.HOME} element={<Home />} />
-                <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
+                <Route element={<BaseScreen />}>
+                    <Route path={ROUTES.HOME} element={<Home />} />
+                </Route>
+
                 <Route
-                    path={ROUTES.ERROR}
-                    element={<Navigate to={ROUTES.NOT_FOUND} replace />}
-                />
-                <Route path={ROUTES.LOGIN} element={<Login />} />
-                <Route path={ROUTES.REGISTER} element={<Register />} />
-                <Route
-                    path={ROUTES.RESET_PASSWORD}
-                    element={<ResetPassword />}
-                />
-                <Route
-                    path={ROUTES.CONFIRMATION_EMAIL}
-                    element={<EmailConfirmation />}
-                />
+                    element={
+                        <LoggedOutRoute>
+                            <BaseScreen />
+                        </LoggedOutRoute>
+                    }
+                >
+                    <Route path={ROUTES.LOGIN} element={<Login />} />
+
+                    <Route path={ROUTES.REGISTER} element={<Register />} />
+
+                    <Route
+                        path={ROUTES.RESET_PASSWORD}
+                        element={<ResetPassword />}
+                    />
+
+                    <Route
+                        path={ROUTES.CONFIRMATION_EMAIL}
+                        element={<EmailConfirmation />}
+                    />
+                </Route>
+
                 <Route
                     element={
                         <ProtectedRoute>
@@ -45,14 +56,25 @@ export const Router = () => {
                     }
                 >
                     <Route path={`${ROUTES.CHAT}/:id`} element={<Chat />} />
+
                     <Route path={ROUTES.FEED} element={<Feed />} />
+
                     <Route path={ROUTES.POST} element={<Post />} />
+
                     <Route
                         path={`${ROUTES.PROFILE}/:id`}
                         element={<Profile />}
                     />
+
                     <Route path={ROUTES.SEARCH} element={<Search />} />
                 </Route>
+
+                <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
+
+                <Route
+                    path='*'
+                    element={<Navigate to={ROUTES.NOT_FOUND} replace />}
+                />
             </Routes>
         </BrowserRouter>
     );
