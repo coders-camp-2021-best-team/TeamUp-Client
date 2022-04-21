9be6e96c2e0072a5e6861572b35eaa0ed74b4dca
Maciej Opaliński <poprostumieciek@gmail.com>
2022-04-21 11:27:29 +0200

    feat: Fetch user profile by username

--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,16 +1,16 @@
 import { Navigate, useParams } from 'react-router-dom';
 
-import { useUser } from '../../Api/EndPoints/useUser';
+import { useUserByUsername } from '../../Api/EndPoints/useUserByUsername';
 import { UserProfile } from '../../components';
 import { ROUTES } from '../../routes/Routes';
 
 export const Profile = () => {
-    const { id } = useParams();
-    const user = useUser(id);
+    const { username } = useParams();
+    const user = useUserByUsername(username || '');
 
     if (user.isLoading) return null;
 
-    if (!user.data) return <Navigate to={ROUTES.NOT_FOUND} />;
+    if (!user.data) return <Navigate to={ROUTES.NOT_FOUND} replace />;
 
     return <UserProfile user={user.data} />;
 };
