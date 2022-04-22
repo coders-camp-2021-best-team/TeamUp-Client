4e6502cc6eea4bc3c09aec800883005f01d48bf6
Maciej Opaliński <poprostumieciek@gmail.com>
2022-04-22 10:12:07 +0200

    fix: Simplify components structure

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,6 @@
 export * from './Header';
+export * from './LoggedOutRoute';
 export * from './Navigation';
-export * from './Profile';
+export * from './PostCard';
 export * from './ProtectedRoute';
+export * from './UserProfileDescription';
--- src/screens/Profile/Profile.tsx
+++ src/screens/Profile/Profile.tsx
@@ -1,8 +1,10 @@
+import { Box, Typography } from '@mui/material';
 import { Navigate, useParams } from 'react-router-dom';
 
 import { useUserByUsername } from '../../Api/EndPoints/useUserByUsername';
-import { UserProfile } from '../../components';
+import { UserProfileDescription } from '../../components';
 import { ROUTES } from '../../routes/Routes';
+import { CDN } from '../../utils/CDN';
 
 export const Profile = () => {
     const { username } = useParams();
@@ -12,5 +14,29 @@ export const Profile = () => {
 
     if (!user.data) return <Navigate to={ROUTES.NOT_FOUND} replace />;
 
-    return <UserProfile user={user.data} />;
+    return (
+        <>
+            <Box
+                component='img'
+                alt='Profile Picture'
+                src={CDN(user.data.avatar || '')}
+                width='100%'
+                height='60vh'
+                sx={{
+                    borderRadius: '6px',
+                    objectFit: 'cover'
+                }}
+            />
+            <Typography
+                variant='h5'
+                sx={{ flexGrow: 1 }}
+                margin='2vh 5.5vw'
+                color='white'
+            >
+                {user.data.first_name} {user.data.last_name},{' '}
+                {user.data.birthdate}
+            </Typography>
+            <UserProfileDescription user={user.data} />
+        </>
+    );
 };
