fdc9e931b6962f6a9b911ad66a65fb171ff0d1d9
Maciej Opaliński <poprostumieciek@gmail.com>
2022-04-25 13:41:51 +0200

    fix: Fix `Navigation` component

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,40 +1,39 @@
 import { MenuItem } from '@mui/material';
 import Link from '@mui/material/Link';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-import { useLogout } from '../Api/EndPoints/useLogout';
+import { useUser } from '../Api/EndPoints/useUser';
 import { ROUTES } from '../routes/Routes';
 
-const NAV_ITEMS = [
-    {
-        label: 'Home',
-        to: ROUTES.FEED
-    },
-    {
-        label: 'Profile',
-        to: `${ROUTES.PROFILE}/username`
-    },
-    {
-        label: 'Chat',
-        to: `${ROUTES.CHAT}/id`
-    },
-    {
-        label: 'Posts',
-        to: ROUTES.POSTS
-    },
-    {
-        label: 'Search',
-        to: ROUTES.SEARCH
-    }
-];
-
 export const Navigation = () => {
-    const navigate = useNavigate();
-    const logout = useLogout();
+    const { data: user } = useUser();
 
-    const handleLogoutClick = () => {
-        logout.mutateAsync().then(() => navigate(ROUTES.HOME));
-    };
+    const NAV_ITEMS = [
+        {
+            label: 'Home',
+            to: ROUTES.FEED
+        },
+        {
+            label: 'Profile',
+            to: `${ROUTES.PROFILE}/${user?.username}`
+        },
+        {
+            label: 'Chats',
+            to: ROUTES.CHAT
+        },
+        {
+            label: 'Posts',
+            to: ROUTES.POSTS
+        },
+        {
+            label: 'Search',
+            to: ROUTES.SEARCH
+        },
+        {
+            label: 'Logout',
+            to: ROUTES.LOGOUT
+        }
+    ];
 
     return (
         <>
@@ -51,17 +50,6 @@ export const Navigation = () => {
                     </Link>
                 </MenuItem>
             ))}
-            <MenuItem>
-                <Link
-                    underline='hover'
-                    variant='body1'
-                    color={'#000'}
-                    component='button'
-                    onClick={handleLogoutClick}
-                >
-                    Logout
-                </Link>
-            </MenuItem>
         </>
     );
 };
