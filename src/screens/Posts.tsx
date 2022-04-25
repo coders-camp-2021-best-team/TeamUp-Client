72f1bb7909f04626c62452e1ba76c80a93974757
Konrad <konradtrawczynski@gmail.com>
2022-04-25 17:54:51 +0200

    fix: Display user age on profile screen (#64)
    * fix: Use `getAge` to display user age
    
    * fix: Post
    
    Co-authored-by: Konrad <trakon1e@gmail.com>

--- src/screens/Posts.tsx
+++ src/screens/Posts.tsx
@@ -1,8 +1,11 @@
 import { Box, Pagination } from '@mui/material';
+import Button from '@mui/material/Button';
 import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import { POSTS_PAGE_SIZE, usePosts } from '../Api/EndPoints/usePosts';
 import { PostCard } from '../components/PostCard';
+import { ROUTES } from '../routes/Routes';
 
 export const Posts = () => {
     const [page, setPage] = useState(1);
@@ -28,6 +31,26 @@ export const Posts = () => {
                 width: '440px'
             }}
         >
+            <NavLink
+                to={`${ROUTES.CREATE_POST}/`}
+                style={{
+                    textDecoration: 'none',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    marginTop: '10px'
+                }}
+            >
+                <Button
+                    variant='contained'
+                    size='small'
+                    type='submit'
+                    sx={{
+                        padding: '5px 60px'
+                    }}
+                >
+                    Add new post
+                </Button>
+            </NavLink>
             {posts.data.map((post) => (
                 <PostCard key={post.id} post={post} />
             ))}
